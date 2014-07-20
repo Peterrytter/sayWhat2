631f8adf748630e7e3eb971e3cdbf0973cc4ad84
peterrytter <Pede23mzt@gmail.com>
2014-07-21 01:47:08 +0200

    sayWhat2

---
+++ Whatever2/src/Whatever1/sayWhat.java
@@ -0,0 +1,16 @@
+package Whatever1;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class sayWhat extends JavaPlugin {
+	
+	
+	public void onEnable(){
+		getLogger().info("Hello World!");
+	}
+	
+	public void onDisable(){
+		
+	}
+
+}
